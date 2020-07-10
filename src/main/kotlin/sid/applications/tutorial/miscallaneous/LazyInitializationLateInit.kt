c10f316b52b5de7fe7545c92fb5d7d236eaa14f7
SidK <sidcool1234@gmail.com>
2020-07-10 14:26:35 +0530

    Adds identifiers for expensiveOperation logging

--- src/main/kotlin/sid/applications/tutorial/miscallaneous/LazyInitializationLateInit.kt
+++ src/main/kotlin/sid/applications/tutorial/miscallaneous/LazyInitializationLateInit.kt
@@ -1,13 +1,14 @@
 package sid.applications.tutorial.miscallaneous
 
 import sid.applications.tutorial.models.ComplexNumber
-import kotlin.math.exp
+
+val initValue: Double by lazy { expensiveOperation("initValue") }
 
 fun lateInit(){ // avoids nullable types
     lateinit var lateComer: ComplexNumber
     expensiveOperation()
 //    println(lateComer)  // compiler error
-    lateComer = ComplexNumber(expensiveOperation(), expensiveOperation())
+    lateComer = ComplexNumber(expensiveOperation("lateComer"), expensiveOperation("lateComer"))
     println(lateComer)
     lateComer = ComplexNumber(expensiveOperation(), expensiveOperation()) // duplicate intialization possible®
     println(lateComer)
@@ -15,7 +16,7 @@ fun lateInit(){ // avoids nullable types
 }
 
 fun lazyInit(){
-    val lazyInit: Double by lazy { expensiveOperation() } // function scoped method, hence values not persisted across method calls
+    val lazyInit: Double by lazy { expensiveOperation("lazyInit") } // function scoped method, hence values not persisted across method calls
     val secondLazy: String by lazy { lazyInit.plus(11).toString() }
 
     println("An Inexpensive operation") // executes inline since eager evaluation
@@ -27,11 +28,11 @@ fun lazyInit(){
     }
 }
 
-fun expensiveOperation(): Double {
-    println("expensive operation starts")
+fun expensiveOperation(identifier: String = "Default"): Double {
+    println("expensive operation starts for $identifier")
     Thread.sleep(1000)
-    println("expensive operation stops")
-    return Math.random()*25
+    println("expensive operation stops for $identifier")
+    return Math.random() * 25
 }
 
 fun main(){
